aa2857c4cc7b97c84adef9b295ef2d105cffd487
Richard Lister <rlister@gmail.com>
2014-06-21 12:19:29 -0400

    add github endpoint; build endpoint uses new hash args to perform

--- app.rb
+++ app.rb
@@ -2,14 +2,38 @@ require 'sinatra'
 require 'resque'
 require './builder'
 
+## simole endpoint as GET /build?repo=org/name:branch&image=...
 get '/build' do
 
   if params[:repo]
-    "building #{params[:repo]} to #{params[:image]}"
-    Resque.enqueue(Builder, *params.values_at(:repo, :image))
+    org, name, branch = params[:repo].gsub(/\.git$/, '').split(/[:\/]/)
+    Resque.enqueue(Builder, {
+      org:    org,
+      name:   name,
+      branch: branch,
+      image:  params[:image],
+    })
+    'ok'
   else
     status 422
     body 'missing required param: repo'
+  end
+
+end
+
+## receive a post-receive hook from github
+post '/github' do
+
+  if env["HTTP_X_GITHUB_EVENT"] == "push"
+    payload = JSON.parse(request.body.read)
+    Resque.enqueue(Builder, {
+      org:    payload['repository']['organization'],
+      name:   payload['repository']['name'],
+      branch: payload['ref'].split('/').last,
+    })
+    'ok'
+  else
+    'ignored'
   end
 
 end
