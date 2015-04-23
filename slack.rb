0989e85b5424e5c46755a29cdc1d1eb818514f67
Richard Lister <rlister+gh@gmail.com>
2015-04-22 22:31:32 -0400

    slack notifier now receives simple message as a string and does not construct from repo details

--- slack.rb
+++ slack.rb
@@ -8,23 +8,19 @@ class Builder
   @name    = ENV.fetch('BUILDER_SLACK_NAME', 'builder')
   @token   = ENV.fetch('BUILDER_SLACK_TOKEN', nil)
 
-  def self.notify_slack(repo, message, ok)
+  def self.notify_slack(message, ok)
     uri = URI.parse("https://#{@team}.slack.com/services/hooks/incoming-webhook?token=#{@token}")
 
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_PEER
 
-    ## slack-formatted link to this commit in github
-    sha_link = "<http://github.com/#{repo.org}/#{repo.name}/commit/#{repo.sha}|#{repo.sha.slice(0,10)}>"
-
     request = Net::HTTP::Post.new(uri.request_uri)
     request.set_form_data(payload: {
       username: @name,
       channel:  @channel,
       attachments: [{
-        # text:      "build #{build_ok ? 'complete' : 'failed'} for #{repo.name}:#{repo.branch} #{sha_link}",
-        text:      "#{message} for #{repo.image} #{sha_link}",
+        text:      message,
         color:     ok ? 'good' : 'danger',
         mrkdwn_in: %w[ text ],  #allow link formatting in attachment
       }]
